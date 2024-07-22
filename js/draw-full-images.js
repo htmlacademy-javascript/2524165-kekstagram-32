5e701b68b736d66e395ce9b0a9571ca3a2b19759
Valentin Tazhetdinov <valentyntm@gmail.com>
2024-07-22 21:41:23 +0300

    Добавил постепенную загрузку комментариев на кнопку.

--- js/draw-full-images.js
+++ js/draw-full-images.js
@@ -1,17 +1,23 @@
 import { generatedPhotos } from './main';
 
 const BUTTON_CLOSE_MODULE_WINDOW = 'Escape';
+const NUMBER_OF_COMMENTS = 5;
 
 const picturesContainer = document.querySelector('.pictures');
 const bigPictureElement = document.querySelector('.big-picture');
+const buttonCommentsLoader = bigPictureElement.querySelector('.comments-loader');
 
 picturesContainer.addEventListener('click', openBigPicture);
 bigPictureElement.querySelector('.cancel').addEventListener('click', closeBigPicture);
+buttonCommentsLoader.addEventListener('click', showMoreComments);
+
+let selectedBigPictureData;
 
 function openBigPicture (evt) {
   if (evt.target.matches('img')) {
     const photoIndex = Array.from(picturesContainer.querySelectorAll('.picture img')).indexOf(evt.target);
     const photoData = generatedPhotos[photoIndex];
+    selectedBigPictureData = photoData;
 
     bigPictureElement.classList.remove('hidden');
     bigPictureElement.querySelector('.big-picture__img img').src = photoData.url;
@@ -25,9 +31,6 @@ function openBigPicture (evt) {
 
     bigPictureSocial.querySelector('.social__caption').textContent = photoData.description;
 
-    bigPictureSocial.querySelector('.social__comment-count').classList.add('hidden');
-    bigPictureSocial.querySelector('.comments-loader').classList.add('hidden');
-
     document.body.classList.add('modal-open');
 
     document.addEventListener('keydown', onDocumentKeydown);
@@ -37,34 +40,55 @@ function openBigPicture (evt) {
 
 function loadComments (comments) {
   const commentsSection = bigPictureElement.querySelector('.social__comments');
+  let commentsCurrentCount = commentsSection.querySelectorAll('.social__comment').length;
+
   const commentsFragment = document.createDocumentFragment();
 
-  comments.forEach(({avatar, message, name}) => {
-    const newComment = document.createElement('li');
-    newComment.classList.add('social__comment');
+  for (let i = commentsCurrentCount; i < commentsCurrentCount + NUMBER_OF_COMMENTS; i++) {
+    if (comments[i]) {
+      const {avatar, name, message} = comments[i];
+      const newComment = document.createElement('li');
+      newComment.classList.add('social__comment');
 
-    newComment.innerHTML = `<img
-      class="social__picture"
-      src="${avatar}"
-      alt="${name}"
-      width="35" height="35">
-      <p class="social__text">${message}</p>`;
+      newComment.innerHTML = `<img
+        class="social__picture"
+        src="${avatar}"
+        alt="${name}"
+        width="35" height="35">
+        <p class="social__text">${message}</p>`;
 
-    commentsFragment.append(newComment);
-  });
+      commentsFragment.append(newComment);
+
+    }
+  }
 
   commentsSection.append(commentsFragment);
+
+  commentsCurrentCount = commentsSection.querySelectorAll('.social__comment').length;
+  if (commentsCurrentCount === comments.length) {
+    buttonCommentsLoader.classList.add('hidden');
+  } else {
+    buttonCommentsLoader.classList.remove('hidden');
+  }
+
+  bigPictureElement.querySelector('.social__comment-shown-count').textContent = commentsCurrentCount;
+
 }
 
 function closeBigPicture () {
   deleteBigPictureComments();
   bigPictureElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
 function deleteBigPictureComments () {
   const comments = bigPictureElement.querySelector('.social__comments');
   comments.replaceChildren();
+}
+
+function showMoreComments () {
+  loadComments(selectedBigPictureData.comments);
 }
 
 function onDocumentKeydown (evt) {
